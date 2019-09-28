3274d6d365c511412f55a18afc1349af7ab8a408
olya <olya_r@tut.by>
2019-09-28 20:19:40 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
+  const CLOSING_BRACKETS = {};
+  const OPENING_BRACKETS = {};
+  
+  for (const bracketsPair of bracketsConfig) {
+    CLOSING_BRACKETS[bracketsPair[0]] = bracketsPair[1];
+    OPENING_BRACKETS[bracketsPair[1]] = bracketsPair[0];
+  }
+
+  const stack = [];
+
+  for (const bracket of str) {
+    if (stack.length != 0 && CLOSING_BRACKETS[stack[stack.length-1]] == bracket) {
+      stack.pop();
+      continue;
+    }
+    if (CLOSING_BRACKETS[bracket]) {
+      stack.push(bracket);
+      continue;
+    }
+    if (OPENING_BRACKETS[bracket]) {
+      return false;
+    }
+  }
+
+  return (stack.length == 0);
   // your solution
 }
